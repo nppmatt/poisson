b7acff5896e4c576e07587526a0d857eae132df5
Matthew Cho <mc462@login02.tartan.njit.edu>
2024-03-14 23:10:57 -0400

    jacobi 10 iter first run

--- src/jacobi.c
+++ src/jacobi.c
@@ -6,41 +6,57 @@
 
 const long GRID_W = 33;
 const long GRID_H = 33;
-const double STEP = 2.0 / (GRID_W - 1);
 const long ITER = 10;
 const double xMin = -1.0;
 const double xMax = 1.0;
 const double yMin = -1.0;
 const double yMax = 1.0;
+const double dx = (xMax - xMin) / (GRID_W - 1);
+const double dy = (yMax - yMin) / (GRID_H - 1);
+const double dxxinv = 1.0 / (dx * dx);
+const double dyyinv = 1.0 / (dy * dy);
+const double dmid = 1.0 / (2.0 * (dxxinv + dyyinv));
 
 /* Source function given an the row and column indices.
  * (Mind that steps are hardcoded for square domains.)
  * Ternary statement taken from Rycroft's header.
  */
 inline double srcFunc(long i, long j) {
-	double x = xMin + i * STEP;
-	double y = yMin + j * STEP;
+	double x = xMin + i * dx;
+	double y = yMin + j * dy;
 	return (fabs(x) > 0.5 || fabs(y) > 0.5)?0:1;
 }
 
-/* 2-D Jacobi approximate given the current state of a grid. */
-void jacobi(double** approx, long iterations) {
+/* 2-D Jacobi approximate given a guess as input. */
+double** jacobi(double** input, long iterations) {
+	double** approx;
+	double** swap;
+	approx = matrix(1, GRID_W, 1, GRID_H);
+	swap = matrix(1, GRID_W, 1, GRID_H);
 	long it, row, col;
-	for (it = 0; it < iterations; ++it) {
+	for (it = 1; it <= iterations; ++it) {
+		if (it % 2 == 0) {
+			swap = input;
+			input = approx;
+			approx = input;
+		}
 		for (row = 2; row <= GRID_H-1; ++row) {
 			for (col = 2; col <= GRID_W-1; ++col) {
-				approx[row][col] = (STEP*STEP*srcFunc(row, col) + 
-						approx[row][col+1] + approx[row][col-1] +
-						approx[row+1][col] + approx[row-1][col]) / 
-						4.0;
+				approx[row][col] = (srcFunc(row, col) + 
+					dxxinv * (approx[row][col+1] + approx[row][col-1]) +
+					dyyinv * (approx[row+1][col] + approx[row-1][col])
+					) * dmid;
 			}
 		}
 	}
+	return approx;
 }
 
 int main() {
-	double** approxMatrix;
-	approxMatrix = matrix(1, GRID_W, 1, GRID_H);
+	double** guess;
+	double** approxOutput;
+	guess = matrix(1, GRID_W, 1, GRID_H);
+	approxOutput = matrix(1, GRID_W, 1, GRID_H);
 
 	long row, col;
 	double xPos, yPos;
@@ -48,21 +64,21 @@ int main() {
 	/* Set source function over grid */
 	for (row = 1; row <= GRID_H; ++row) {
 		for (col = 1; col <= GRID_W; ++col) {
-			approxMatrix[row][col] = 0.0;
+			guess[row][col] = 0.0;
 		}
 	}
 
-	jacobi(approxMatrix, 10);
+	approxOutput = jacobi(guess, 10);
 
 
 	/* Print data as CSV with last row to be truncated */
 	for (row = 1; row <= GRID_H; ++row) {
 		for (col = 1; col <= GRID_W; ++col) {
 			if (col == GRID_W) {
-				printf("%f", approxMatrix[row][col]);
+				printf("%f", approxOutput[row][col]);
 			}
 			else {
-				printf("%f,", approxMatrix[row][col]);
+				printf("%f,", approxOutput[row][col]);
 			}
 		}
 		printf("\n");
